510b3c026a9718c517e37890e6ae3ae7cd94bd63
pachungulo <115584328+pachungulo@users.noreply.github.com>
2024-11-23 14:30:41 -0500

    Added form for classes + splitting

--- app.py
+++ app.py
@@ -2,15 +2,14 @@ from flask import Flask, render_template, url_for, request, redirect
 
 app = Flask(__name__)
 
-@app.route('/', methods=["POST", "GET"])
+@app.route('/')
 def index():
+    return render_template("index.html")
+
+@app.route('/result', methods=["POST", "GET"])
+def submit():
     if request.method == "POST":
-        classes = request.form["classes"]
-        return redirect(url_for('submit', classes=classes))
-
-    else:
-        return render_template("index.html")
-
-@app.route('/submit/<classes>')
-def submit(classes):
-    return render_template("submit.html", classes=classes)
+        result = request.form
+        classes = result["classes"]
+        classes = classes.split(",")
+        return render_template("submit.html", classes=classes)
